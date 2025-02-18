0e22c2e81d351463910bcbae567c269798d7f0ab
Heron Dias dos Santos <113992903+herondiassantos@users.noreply.github.com>
2025-02-17 21:09:26 -0300

    Altera o Server

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders logo da AluraBooks', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logoElement = screen.getByAltText(/Logo da AluraBooks/i);
+  expect(logoElement).toBeInTheDocument();
 });
